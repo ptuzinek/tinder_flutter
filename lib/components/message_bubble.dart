1ece8b0fc66048a013a9e261bbfd6b98e7c4a322
ptuzinek <przeo1996@gmail.com>
2020-11-02 12:48:37 +0100

    Added functionality to the ChatScreen and started working on UI

---
+++ lib/components/message_bubble.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  final String messageText;
+  final String messageSender;
+
+  const MessageBubble({this.messageText, this.messageSender});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            'sender: $messageSender',
+            style: TextStyle(
+              fontSize: 11,
+              color: Colors.grey,
+            ),
+          ),
+          Material(
+            color: Colors.orangeAccent,
+            elevation: 5,
+            borderRadius: BorderRadius.circular(30),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                '$messageText',
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tinder_flutter/components/message_bubble.dart';
+import 'package:tinder_flutter/constants.dart';
 import 'package:tinder_flutter/screens/chat_wall_screen.dart';
+
+final _firestore = FirebaseFirestore.instance;
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -11,11 +16,20 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
   User loggedUser;
+  String messageText;
+
+  final messageTextController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     getUser();
+  }
+
+  void messageStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {}
+    }
   }
 
   void getUser() {
@@ -38,11 +52,78 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
             icon: Icon(Icons.close),
             onPressed: () {
-              Navigator.pushReplacementNamed(context, ChatWallScreen.id);
+              Navigator.pop(context);
             },
           )
         ],
+        title: Text('⚡️Chat'),
+        backgroundColor: Colors.deepOrangeAccent,
       ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MessagesStream(),
+            Container(
+              decoration: kMessageContainerDecoration,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: messageTextController,
+                      onChanged: (value) {
+                        messageText = value;
+                      },
+                      decoration: kMessageTextFieldDecoration,
+                    ),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      messageTextController.clear();
+                      _firestore.collection('messages').add(
+                          {'text': messageText, 'sender': loggedUser.email});
+                    },
+                    child: Text(
+                      'Send',
+                      style: kSendButtonTextStyle,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        List<MessageBubble> messageWidgets = [];
+        if (snapshot.hasData) {
+          final messages = snapshot.data.docs;
+          for (var message in messages) {
+            final messageText = message.get('text');
+            final messageSender = message.get('sender');
+            final messageWidget = MessageBubble(
+                messageText: messageText, messageSender: messageSender);
+            messageWidgets.add(messageWidget);
+          }
+        }
+        return Expanded(
+          child: ListView(
+            padding: EdgeInsets.all(10),
+            children: messageWidgets,
+          ),
+        );
+      },
     );
   }
 }
